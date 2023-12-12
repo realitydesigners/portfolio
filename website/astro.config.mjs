cc48d6473113db5f15acc9e6c72540bdd57afa33
RD <raymond.luke.spartz@gmail.com>
2023-12-12 11:32:09 -0800

    test

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,7 +5,6 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
    site: 'https://example.com',
    integrations: [mdx(), sitemap(), react(), tailwind()],
