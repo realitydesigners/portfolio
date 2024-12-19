09789bc1bd09bfe3d05e179c72336db780927188
Raymond <raymond.luke.spartz@gmail.com>
2024-12-19 10:10:29 -0600

    dep

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  site: "https://raymondreamer.com",
+  integrations: [mdx(), sitemap(), react(), tailwind()],
+});
