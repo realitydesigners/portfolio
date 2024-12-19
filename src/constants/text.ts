cdb6495e48a6b8bfd9b37affc0522b3a9541645c
Raymond <raymond.luke.spartz@gmail.com>
2024-12-19 10:42:37 -0600

    improving

---
+++ src/constants/text.ts
@@ -0,0 +1,40 @@
+export const skills = [
+  "Next.js",
+  "Typescript",
+  "Nextjs 15",
+
+  "React 19",
+
+  "Astro",
+  "React",
+  "Tailwind",
+  "Three.js",
+  "React-Three-Fiber",
+  "Sanity CMS",
+  "Spline",
+  "3D Animation",
+  "Figma",
+];
+
+export const socialLinks = [
+  { name: "YouTube", url: "https://www.youtube.com/@realitydesigners" },
+  { name: "Twitter", url: "https://twitter.com/realitydesignrs" },
+  { name: "Instagram", url: "https://www.instagram.com/raymondreamer" },
+  { name: "Github", url: "https://github.com/realitydesigners" },
+  { name: "Portfolio", url: "https://app.spline.design/@realitydesigners" },
+];
+
+export const projects = [
+  {
+    title: "Reality Designers",
+    url: "https://reality-designers.com",
+    image: "/images/Reality-Designers.png",
+    alt: "Reality Designers",
+  },
+  {
+    title: "Rythm Algorithmic Day Trading Strategies",
+    url: "https://rythm.capital",
+    image: "/images/Rythm.png",
+    alt: "Rythm",
+  },
+];
