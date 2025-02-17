f22b00695b8e278915bb536dc99131412c1a5c40
Raymond <raymond.luke.spartz@gmail.com>
2025-02-16 18:28:43 -0800

    spline

---
+++ src/components/SplineScene.jsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function SplineScene() {
+  return (
+    <Spline scene="https://prod.spline.design/TmXL0dRWB4pSsfsm/scene.splinecode" />
+  );
+}
